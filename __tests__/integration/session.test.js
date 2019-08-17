850e6a6b25c44ba58d214b6fa64c062225f010d4
Guilherme Andrade <andrade@rarolabs.com.br>
2019-08-17 16:56:26 -0300

    novos testes, acrescimo do faker, configuracoes para report

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,12 +1,101 @@
-const { User } = require('../../src/app/models/');
+const request = require('supertest');
+const app = require('../../src/app');
+const truncate = require('../utils/truncate');
+// const { User } = require('../../src/app/models/');
+const factory = require('../factories');
 describe('Authentication', () => {
-  it('should sum two numbers', async () => {
-    const user = await User.create({
-      name: 'Guilherme Andrade',
-      email: 'guilhermeaandrade91@gmail.com',
-      password_hash: '12345678'
-    })
-    console.log(user);
-    expect(user.email).toBe('guilhermeaandrade91@gmail.com')
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should authenticate with valid credentials', async () => {
+    const user = await factory.create('User', {
+      password: '123123'
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123123"
+      });
+    expect(response.status).toBe(200);
+  });
+
+  it('should not authenticate with invalid credentials', async () => {
+    const user = await factory.create('User', {
+      password: '123123'
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123128"
+      });
+    expect(response.status).toBe(401);
+  });
+
+  it('should not authenticate if user not exists', async () => {
+    const user = await factory.create('User', {
+      password: '123123'
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: 'test@gmail.com',
+        password: "123123"
+      });
+    expect(response.status).toBe(404);
+  });
+
+  it('should return jwt token when authenticated', async () => {
+    const user = await factory.create('User', {
+      password: '123123'
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123123"
+      });
+    expect(response.body).toHaveProperty("token");
+  });
+
+  it('should be able to access private routes when authenticated', async () => {
+    const user = await factory.create('User', {
+      password: '123123'
+    });
+
+    const response = await request(app)
+      .get('/dashboard')
+      .set('Authorization', `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should not be able to access private routes whitout jwt token', async () => {
+    const user = await factory.create('User', {
+      password: '123123'
+    });
+
+    const response = await request(app)
+      .get('/dashboard');
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should not be able to access private routes with jwt token invalid', async () => {
+    const user = await factory.create('User', {
+      password: '123123'
+    });
+
+    const response = await request(app)
+      .get('/dashboard')
+      .set('Authorization', `Bearer 1234567`);
+
+    expect(response.status).toBe(401);
   });
 });
---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,22 @@
+const { User } = require("../models");
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).send({ message: 'Incorret password' });
+    }
+
+    return res.send({
+      user,
+      token: user.generateToken()
+    });
+  }
+}
+
+module.exports = new SessionController();
